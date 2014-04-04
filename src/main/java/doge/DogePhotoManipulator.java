8d3e22fe0a27cac3f2fc7ab5b1aa8cb6ff287f81
Phillip Webb <pwebb@gopivotal.com>
2014-04-03 22:43:09 -0700

    Doge hacking

--- src/main/java/doge/DogePhotoManipulator.java
+++ src/main/java/doge/DogePhotoManipulator.java
@@ -1,7 +1,8 @@
 
 package doge;
 
-import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
@@ -16,10 +17,29 @@ import javax.imageio.ImageIO;
  */
 public class DogePhotoManipulator implements PhotoManipulator {
 
+	private static final int IMAGE_WIDTH = 300;
+
+	private final BufferedImage overlayTop;
+
+	private final BufferedImage overlayBottom;
+
+	public DogePhotoManipulator() {
+		this.overlayTop = readClassImage("doge-top.png");
+		this.overlayBottom = readClassImage("doge-bottom.png");
+	}
+
+	private BufferedImage readClassImage(String name) {
+		try {
+			return ImageIO.read(getClass().getResourceAsStream(name));
+		} catch (IOException ex) {
+			throw new IllegalStateException(ex);
+		}
+	}
+
 	public Photo manipulate(Photo photo) throws IOException {
-		BufferedImage image = readImage(photo);
-		manipulate(image);
-		return new BufferedImagePhoto(image);
+		BufferedImage sourceImage = readImage(photo);
+		BufferedImage destinationImage = manipulate(sourceImage);
+		return new BufferedImagePhoto(destinationImage);
 	}
 
 	private BufferedImage readImage(Photo photo) throws IOException {
@@ -31,10 +51,45 @@ public class DogePhotoManipulator implements PhotoManipulator {
 		}
 	}
 
-	private void manipulate(BufferedImage image) {
-		Graphics g = image.getGraphics();
-		g.setFont(g.getFont().deriveFont(30f));
-		g.drawString("Hello World!", 100, 100);
-		g.dispose();
+	private BufferedImage manipulate(BufferedImage sourceImage) {
+		double aspectRatio = sourceImage.getHeight() / (double) sourceImage.getWidth();
+		int height = (int) Math.floor(IMAGE_WIDTH * aspectRatio);
+		BufferedImage destinationImage = new BufferedImage(IMAGE_WIDTH, height,
+				BufferedImage.TYPE_INT_RGB);
+		render(sourceImage, destinationImage);
+		return destinationImage;
+	}
+
+	private void render(BufferedImage sourceImage, BufferedImage destinationImage) {
+		Graphics2D destinationGraphics = destinationImage.createGraphics();
+		try {
+			setGraphicsHints(destinationGraphics);
+			renderBackground(sourceImage, destinationImage, destinationGraphics);
+			renderOverlay(destinationImage, destinationGraphics);
+		} finally {
+			destinationGraphics.dispose();
+		}
+	}
+
+	private void renderBackground(BufferedImage sourceImage,
+			BufferedImage destinationImage, Graphics2D destinationGraphics) {
+		destinationGraphics.drawImage(sourceImage, 0, 0, IMAGE_WIDTH,
+				destinationImage.getHeight(), null);
+	}
+
+	private void renderOverlay(BufferedImage destinationImage,
+			Graphics2D destinationGraphics) {
+		destinationGraphics.drawImage(this.overlayTop, 0, 0, null);
+		int y = destinationImage.getHeight() - this.overlayBottom.getHeight();
+		destinationGraphics.drawImage(this.overlayBottom, 0, y, null);
+	}
+
+	private void setGraphicsHints(Graphics2D graphics) {
+		graphics.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
+				RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+		graphics.setRenderingHint(RenderingHints.KEY_RENDERING,
+				RenderingHints.VALUE_RENDER_QUALITY);
+		graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+				RenderingHints.VALUE_ANTIALIAS_ON);
 	}
 }
--- src/test/java/doge/DogePhotoManipulatorTest.java
+++ src/test/java/doge/DogePhotoManipulatorTest.java
@@ -1,8 +1,6 @@
 
 package doge;
 
-import static org.junit.Assert.assertTrue;
-
 import java.io.File;
 import java.io.FileOutputStream;
 
