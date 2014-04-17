e40f7784a10d7359013512f0510042547399a2f2
Josh Long <josh@joshlong.com>
2014-04-17 18:56:28 +0200

    updated the GridFsTemplate bits

--- spring-doge/src/main/java/doge/Application.java
+++ spring-doge/src/main/java/doge/Application.java
@@ -16,23 +16,22 @@
 
 package doge;
 
-import java.net.InetSocketAddress;
-import java.net.Socket;
-import java.util.concurrent.TimeUnit;
-
-import javax.servlet.MultipartConfigElement;
-
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.graphite.Graphite;
+import com.codahale.metrics.graphite.GraphiteReporter;
+import com.mongodb.Mongo;
+import com.mongodb.MongoException;
+import com.mongodb.MongoURI;
+import doge.domain.User;
+import doge.domain.UserRepository;
+import doge.photo.DogePhotoManipulator;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoProperties;
 import org.springframework.boot.context.embedded.MultiPartConfigFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Condition;
-import org.springframework.context.annotation.ConditionContext;
-import org.springframework.context.annotation.Conditional;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.*;
 import org.springframework.core.type.AnnotatedTypeMetadata;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
@@ -44,15 +43,10 @@ import org.springframework.web.socket.config.annotation.AbstractWebSocketMessage
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 
-import com.codahale.metrics.MetricRegistry;
-import com.codahale.metrics.graphite.Graphite;
-import com.codahale.metrics.graphite.GraphiteReporter;
-import com.mongodb.Mongo;
-import com.mongodb.MongoException;
-
-import doge.domain.User;
-import doge.domain.UserRepository;
-import doge.photo.DogePhotoManipulator;
+import javax.servlet.MultipartConfigElement;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Application configuration and main method.
@@ -65,108 +59,108 @@ import doge.photo.DogePhotoManipulator;
 @EnableAutoConfiguration
 public class Application {
 
-	@Bean
-	public MultipartConfigElement multipartConfigElement() {
-		MultiPartConfigFactory factory = new MultiPartConfigFactory();
-		factory.setMaxFileSize("10Mb");
-		return factory.createMultipartConfig();
-	}
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultiPartConfigFactory factory = new MultiPartConfigFactory();
+        factory.setMaxFileSize("10Mb");
+        return factory.createMultipartConfig();
+    }
 
-	@Bean
-	public WebMvcConfigurerAdapter mvcViewConfigurer() {
-		return new WebMvcConfigurerAdapter() {
-			@Override
-			public void addViewControllers(ViewControllerRegistry registry) {
-				registry.addViewController("/client").setViewName("client");
-				registry.addViewController("/monitor").setViewName("monitor");
-			}
-		};
-	}
+    @Bean
+    public WebMvcConfigurerAdapter mvcViewConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addViewControllers(ViewControllerRegistry registry) {
+                registry.addViewController("/client").setViewName("client");
+                registry.addViewController("/monitor").setViewName("monitor");
+            }
+        };
+    }
 
-	@Bean
-	public DogePhotoManipulator dogePhotoManipulator() {
-		return new DogePhotoManipulator();
-	}
+    @Bean
+    public DogePhotoManipulator dogePhotoManipulator() {
+        return new DogePhotoManipulator();
+    }
 
-	@Bean
-	public GridFsTemplate gridFsTemplate(Mongo mongo, MongoTemplate mongoTemplate) {
-		return new GridFsTemplate(new SimpleMongoDbFactory(mongo, "fs"),
-				mongoTemplate.getConverter());
-	}
+    @Bean
+    public GridFsTemplate gridFsTemplate(MongoProperties properties, MongoTemplate mongo)
+            throws Exception {
+        MongoURI mongoURI = new MongoURI(properties.getUri());
+        SimpleMongoDbFactory simpleMongoDbFactory = new SimpleMongoDbFactory(mongoURI);
+        return new GridFsTemplate(simpleMongoDbFactory, mongo.getConverter());
+    }
 
-	@Bean
-	public InitializingBean populateTestData(UserRepository repository) {
-		return () -> {
-			repository.save(new User("philwebb", "Phil Webb"));
-			repository.save(new User("joshlong", "Josh Long"));
-			repository.findAll().forEach(System.err::println);
-		};
-	}
+    @Bean
+    public InitializingBean populateTestData(UserRepository repository) {
+        return () -> {
+            repository.save(new User("philwebb", "Phil Webb"));
+            repository.save(new User("joshlong", "Josh Long"));
+            repository.findAll().forEach(System.err::println);
+        };
+    }
 
-	@Bean
-	public HealthIndicator<Object> mongoHealthIndicator(Mongo mongo) {
-		return () -> {
-			try {
-				mongo.getDatabaseNames();
-				return "ok";
-			}
-			catch (MongoException ex) {
-				return "error";
-			}
-		};
-	}
+    @Bean
+    public HealthIndicator<Object> mongoHealthIndicator(Mongo mongo) {
+        return () -> {
+            try {
+                mongo.getDatabaseNames();
+                return "ok";
+            } catch (MongoException ex) {
+                return "error";
+            }
+        };
+    }
 
-	@Configuration
-	@EnableWebSocketMessageBroker
-	static class WebSocketConfiguration extends AbstractWebSocketMessageBrokerConfigurer {
+    @Configuration
+    @EnableWebSocketMessageBroker
+    static class WebSocketConfiguration extends AbstractWebSocketMessageBrokerConfigurer {
 
-		@Override
-		public void registerStompEndpoints(StompEndpointRegistry registry) {
-			registry.addEndpoint("/doge").withSockJS();
-		}
+        @Override
+        public void registerStompEndpoints(StompEndpointRegistry registry) {
+            registry.addEndpoint("/doge").withSockJS();
+        }
 
-		@Override
-		public void configureMessageBroker(MessageBrokerRegistry registry) {
-			registry.enableSimpleBroker("/queue/", "/topic/");
-			registry.setApplicationDestinationPrefixes("/app");
-		}
+        @Override
+        public void configureMessageBroker(MessageBrokerRegistry registry) {
+            registry.enableSimpleBroker("/queue/", "/topic/");
+            registry.setApplicationDestinationPrefixes("/app");
+        }
 
-	}
+    }
 
-	@Configuration
-	static class MetricsConfiguration {
+    @Configuration
+    static class MetricsConfiguration {
 
-		private static final InetSocketAddress ADDRESS = new InetSocketAddress(
-				"localhost", 2003);
+        private static final InetSocketAddress ADDRESS = new InetSocketAddress(
+                "localhost", 2003);
 
-		@Bean
-		@Conditional(GraphiteCondition.class)
-		public GraphiteReporter graphiteReporter(MetricRegistry registry) {
-			GraphiteReporter reporter = GraphiteReporter.forRegistry(registry)
-					.prefixedWith("doge.spring.io").build(new Graphite(ADDRESS));
-			reporter.start(2, TimeUnit.SECONDS);
-			return reporter;
-		}
+        @Bean
+        @Conditional(GraphiteCondition.class)
+        public GraphiteReporter graphiteReporter(MetricRegistry registry) {
+            GraphiteReporter reporter = GraphiteReporter.forRegistry(registry)
+                    .prefixedWith("doge.spring.io").build(new Graphite(ADDRESS));
+            reporter.start(2, TimeUnit.SECONDS);
+            return reporter;
+        }
 
-		public static class GraphiteCondition implements Condition {
-			@Override
-			public boolean matches(ConditionContext context,
-					AnnotatedTypeMetadata metadata) {
-				Socket socket = new Socket();
-				try {
-					socket.connect(ADDRESS);
-					socket.close();
-					return true;
-				}
-				catch (Exception ex) {
-					return false;
-				}
-			}
-		}
-	}
+        public static class GraphiteCondition implements Condition {
+            @Override
+            public boolean matches(ConditionContext context,
+                                   AnnotatedTypeMetadata metadata) {
+                Socket socket = new Socket();
+                try {
+                    socket.connect(ADDRESS);
+                    socket.close();
+                    return true;
+                } catch (Exception ex) {
+                    return false;
+                }
+            }
+        }
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
 }
