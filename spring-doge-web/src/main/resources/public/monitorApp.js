8b80acba32187ea51e94b3a2bcb36c2b37e36dba
Josh Long <josh@joshlong.com or josh.long@springsource.com>
2014-09-07 16:04:49 -0700

    Restored animation and angular.js layout bits. Also, added the bits so you could run this in a classic Servlet 3 container if you had to.

--- spring-doge-web/src/main/resources/public/monitorApp.js
+++ spring-doge-web/src/main/resources/public/monitorApp.js
@@ -4,72 +4,70 @@ var appName = 'monitor';
  * Displays newly uploaded images communicated using websockets
  */
 require.config({
-	paths: {
-		doge: 'doge',
-		stomp: doge.jsUrl('stomp-websocket/lib/stomp'),
-		sockjs: doge.jsUrl('sockjs/sockjs'),
-		angular: doge.jsUrl('angular/angular'),
-		domReady: doge.jsUrl('requirejs-domready/domReady')
-	},
-	shim: {
-		angular: {
-			exports: 'angular'
-		}
-	}
+    paths: {
+        doge: 'doge',
+        stomp: doge.jsUrl('stomp-websocket/lib/stomp'),
+        sockjs: doge.jsUrl('sockjs/sockjs'),
+        angular: doge.jsUrl('angular/angular'),
+        domReady: doge.jsUrl('requirejs-domready/domReady')
+    },
+    shim: {
+        angular: {
+            exports: 'angular'
+        }
+    }
 });
 
 define([ 'require', 'angular' ], function (require, angular) {
-	'use strict';
-	require([ 'sockjs', 'angular', 'stomp', 'domReady!' ], function (sockjs, angular, stomp) {
-		angular.bootstrap(document, [ appName ]);
-	});
+    'use strict';
+    require([ 'sockjs', 'angular', 'stomp', 'domReady!' ], function (sockjs, angular, stomp) {
+        angular.bootstrap(document, [ appName ]);
+    });
 
-	var doge = angular.module(appName, []);
+    var doge = angular.module(appName, []);
 
-	doge.controller('MonitorController', [
-		'$scope',
-		'$http',
-		'$log',
-		function ($scope, $http, $log) {
+    doge.controller('MonitorController', [
+        '$scope',
+        '$http',
+        '$log',
+        function ($scope, $http, $log) {
 
-			$scope.imgSource = "";
+            $scope.imgSource = "";
+            $scope.uploads = [];
+            $scope.size = 0;
 
-			require([ 'sockjs', 'stomp' ], function (sockjs, stomp) {
-				var socket = new SockJS('/doge');
-				var client = Stomp.over(socket);
-				client.connect({}, function (frame) {
-					console.log('Connected ' + frame);
-					client.subscribe("/topic/alarms", function (message) {
-						var body = JSON.parse(message.body);
-						$scope.$apply(function () {
-							$scope.onDoge(body);
-						});
-					});
-				}, function (error) {
-					console.log("STOMP protocol error " + error);
-				});
-			});
+            require([ 'sockjs', 'stomp' ], function (sockjs, stomp) {
+                var socket = new SockJS('/doge');
+                var client = Stomp.over(socket);
+                client.connect({}, function (frame) {
+                    console.log('Connected ' + frame);
+                    client.subscribe("/topic/alarms", function (message) {
+                        var body = JSON.parse(message.body);
+                        $scope.$apply(function () {
+                            $scope.onDoge(body);
+                        });
+                    });
+                }, function (error) {
+                    console.log("STOMP protocol error " + error);
+                });
+            });
 
-			$scope.createImage = function (src, alt, title) {
-				var img = document.createElement('img');
-				img.src = src;
-				if (alt != null)
-					img.alt = alt;
-				if (title != null)
-					img.title = title;
-				return img;
-			};
+          /*  $scope.createImage = function (src, alt, title) {
+                var img = document.createElement('img');
+                img.src = src;
+                if (alt != null)
+                    img.alt = alt;
+                if (title != null)
+                    img.title = title;
+                return img;
+            };*/
 
-			$scope.onDoge = function (msg) {
-				var element = document.getElementById('imgPreview');
-				var image = $scope.createImage(msg.dogePhotoUri, 'Such Doge!', 'Such Boot!');
-				image.style.visibility = "hidden";
-				element.insertBefore(image, element.firstChild);
-				image.className = "fadeIn";
-			};
-
-			$scope.imageContainer = null;
+            $scope.onDoge = function (msg) {
+                $scope.uploads.push(msg);
+                $scope.size = $scope.uploads.length
+            };
 
 
-		} ]);
+
+        } ]);
 });
