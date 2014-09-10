15d93f7b7fb3c1cf4df732ad09a0e507845a84f4
Phillip Webb <pwebb@pivotal.io>
2014-09-09 20:30:52 -0500

    Add graphite support using coda metrics

--- spring-doge/src/main/java/doge/Application.java
+++ spring-doge/src/main/java/doge/Application.java
@@ -16,18 +16,30 @@
 
 package doge;
 
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.type.AnnotatedTypeMetadata;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.graphite.Graphite;
+import com.codahale.metrics.graphite.GraphiteReporter;
 
 import doge.domain.User;
 import doge.domain.UserRepository;
@@ -89,6 +101,38 @@ public class Application {
 			registry.enableSimpleBroker("/topic/");
 		}
 
+	}
+
+	@Configuration
+	static class MetricsConfiguration {
+
+		private static final InetSocketAddress ADDRESS = new InetSocketAddress(
+				"localhost", 2003);
+
+		@Bean
+		@Conditional(GraphiteCondition.class)
+		public GraphiteReporter graphiteReporter(MetricRegistry registry) {
+			GraphiteReporter reporter = GraphiteReporter.forRegistry(registry)
+					.prefixedWith("doge.spring.io").build(new Graphite(ADDRESS));
+			reporter.start(2, TimeUnit.SECONDS);
+			return reporter;
+		}
+
+		public static class GraphiteCondition implements Condition {
+			@Override
+			public boolean matches(ConditionContext context,
+					AnnotatedTypeMetadata metadata) {
+				Socket socket = new Socket();
+				try {
+					socket.connect(ADDRESS);
+					socket.close();
+					return true;
+				}
+				catch (Exception ex) {
+					return false;
+				}
+			}
+		}
 	}
 
 	public static void main(String[] args) {
